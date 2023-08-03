bc46534529ce41f7730fa896bd4a8d4856800b1b
unknown <fransenjeanpaul@gmail.com>
2023-08-03 19:11:06 +0200

    eerste versie, aanbieding zonder afronding

---
+++ tijdelijk.py
@@ -0,0 +1,8 @@
+prijzen = {
+    "aardbei": 3,
+    "vanille": 4,
+    "chocolade": 5
+}
+aanbieding = prijzen ["aardbei"] * 0.8
+reclame_tekst = "Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € "
+print(f"{reclame_tekst}{aanbieding}")
