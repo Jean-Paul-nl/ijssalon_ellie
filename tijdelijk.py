340694e1e20275fab7c92c82e0b3e2dcb1d85511
unknown <fransenjeanpaul@gmail.com>
2023-08-04 14:00:13 +0200

    versie 6, scripts dd03082023 niet gebruiken

--- tijdelijk.py
+++ tijdelijk.py
@@ -6,5 +6,6 @@ prijzen = {
 aanbieding = prijzen ["aardbei"] * 0.8
 reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding}"
 reclame_tekst2 = reclame_tekst[0:63]
-print(reclame_tekst2)
-# dit script vervangt de inhoud van commit genummerd d8d77b3
+reclame_tekst3 = reclame_tekst2.upper()
+print(reclame_tekst3)
+# dit script vervangt de inhoud van commit genummerd f42a368
