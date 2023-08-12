0cd79b5773379e9ff0d279d4b0c7c3dabcb3a3a5
unknown <fransenjeanpaul@gmail.com>
2023-08-12 18:09:04 +0200

    functies _1 en _2 toegevoegd

---
+++ algemene_functies.py
@@ -0,0 +1,7 @@
+def mijn_functie_1(Argument):
+    Teruggeefwaarde_1 = pow (Argument, 2)
+    return Teruggeefwaarde_1
+#left open
+def mijn_functie_2(Argument1, Argument2):
+    Teruggeefwaarde_2 = [Argument1 + Argument2, Argument1 - Argument2, Argument1 * Argument2, int(Argument1 / Argument2)]  
+    return Teruggeefwaarde_2
